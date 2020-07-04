b8434508cd92eeb83015226d50242b883df16382
Khronos31 <39792509+Khronos31@users.noreply.github.com>
2020-07-04 12:52:59 +0900

    add libnghttp2

---
+++ packages/libnghttp2/make.sh
@@ -0,0 +1,27 @@
+pkgname=libnghttp2
+pkgver=1.41.0
+pkgrel=1
+
+# URL of source archive
+source="https://github.com/nghttp2/nghttp2/releases/download/v${pkgver}/nghttp2-${pkgver}.tar.gz"
+
+ARCH="${ARCH-$(arch)}"
+
+build() {
+  cd "nghttp2-${pkgver}"
+
+  ./configure \
+    --build=aarch64-apple-darwin \
+    --prefix=/usr \
+    --disable-silent-rules \
+    --disable-python-bindings \
+    --enable-lib-only
+  make
+}
+
+package() {
+  cd "nghttp2-${pkgver}/lib"
+
+  make DESTDIR="${pkgdir}" install
+  install -Dm644 ../COPYING "${pkgdir}/usr/share/licenses/${pkgname}/COPYING"
+}
