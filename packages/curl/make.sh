3934f04256058b75c2a8067f96db5b6a2fa8d146
Khronos31 <39792509+Khronos31@users.noreply.github.com>
2020-07-05 11:26:18 +0900

    fix path to dependencies

--- packages/curl/make.sh
+++ packages/curl/make.sh
@@ -1,6 +1,6 @@
 pkgname=curl
 pkgver=7.71.1
-pkgrel=1
+pkgrel=2
 
 # URL of source archive
 source="https://curl.haxx.se/download/${pkgname}-${pkgver}.tar.gz"
@@ -12,15 +12,15 @@ build() {
 
   ./configure \
     --build=aarch64-apple-darwin \
-    --prefix=/usr \
+    --prefix=/usr/local \
     --disable-debug \
     --disable-dependency-tracking \
     --disable-silent-rules \
     --with-secure-transport \
     --with-ssl=/usr/local \
-    --with-libssh2=/usr \
+    --with-libssh2=/usr/local \
     --with-libidn2=/usr/local \
-    --with-nghttp2=/usr \
+    --with-nghttp2=/usr/local \
     --with-ca-bundle=/usr/local/ssl/cert.pem \
     --with-ca-path=/usr/local/ssl/certs
 
@@ -33,7 +33,7 @@ package() {
   make DESTDIR="${pkgdir}" install
   make DESTDIR="${pkgdir}" install -C scripts
 
-  install -Dm755 lib/mk-ca-bundle.pl "${pkgdir}/usr/libexec/mk-ca-bundle.pl"
+  install -Dm755 lib/mk-ca-bundle.pl "${pkgdir}/usr/local/libexec/mk-ca-bundle.pl"
 
   install -Dm644 COPYING "${pkgdir}/usr/share/licenses/${pkgname}/COPYING"
 }
