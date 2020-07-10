072c3ba15d78b533146e90c8b4de2dc5c1e8a6ea
Khronos31 <39792509+Khronos31@users.noreply.github.com>
2020-07-11 03:12:58 +0900

    add nim

---
+++ packages/nim/make.sh
@@ -0,0 +1,59 @@
+pkgname=nim
+pkgver=1.2.4
+pkgrel=1
+
+# URL of source archive
+source="https://nim-lang.org/download/${pkgname}-${pkgver}.tar.gz"
+
+ARCH="${ARCH-$(arch)}"
+
+build() {
+  cd "${pkgname}-${pkgver}"
+
+  local cpu
+  if [ "${ARCH}" = armv7 ]; then
+    cpu=arm
+  else
+    cpu="${ARCH}"
+  fi
+
+  nim compile --os:macosx --cpu:"${cpu}" -d:release --opt:size koch
+  ./koch boot --os:macosx --cpu:"${cpu}" -d:release --opt:size -d:useLinenoise
+  ./bin/nim compile --os:macosx --cpu:"${cpu}" -d:release --opt:size koch
+  ./koch tools --os:macosx --cpu:"${cpu}" -d:release --opt:size
+}
+
+package() {
+  cd "${pkgname}-${pkgver}" 
+
+  local bindir="${pkgdir}/usr/bin"
+  install -d "${bindir}"
+  install -m755 bin/* "${bindir}"
+
+  local configdir="${pkgdir}/etc/nim"
+  install -d "${configdir}"
+  install -m644 config/* "${configdir}"
+
+  local libdir="${pkgdir}/usr/lib/nim"
+  install -d "${libdir}"
+  cp -R lib/. "${libdir}"
+
+  local docdir="${pkgdir}/usr/share/nim/doc"
+  install -d "${docdir}"
+  cp -R doc/. "${docdir}"
+  rm -rf "${docdir}/html"
+  cp -R examples "${docdir}"
+
+  install -d "${pkgdir}/usr/share/bash-completion/completions"
+  install -m644 tools/nim.bash-completion "${pkgdir}/usr/share/bash-completion/completions/nim"
+  install -m644 dist/nimble/nimble.bash-completion "${pkgdir}/usr/share/bash-completion/completions/nimble"
+
+  install -d "${pkgdir}/usr/share/zsh/vendor-completions"
+  install -m644 tools/nim.zsh-completion "${pkgdir}/usr/share/zsh/vendor-completions/_nim"
+  install -m644 dist/nimble/nimble.zsh-completion "${pkgdir}/usr/share/zsh/vendor-completions/_nimble"
+
+  install -d "${pkgdir}/usr/share/licenses/${pkgname}"
+  install -m644 copying.txt "${pkgdir}/usr/share/licenses/${pkgname}"
+  install -d "${pkgdir}/usr/share/licenses/nimble"
+  install -m644 dist/nimble/license.txt "${pkgdir}/usr/share/licenses/nimble"
+}
